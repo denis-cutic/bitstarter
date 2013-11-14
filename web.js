1b4373fd57deda5acc39dbca7b124839c17a64a8
Denis <denis.cutic@gmail.com>
2013-11-14 15:24:21 +0000

    Reading from index

--- web.js
+++ web.js
@@ -1,9 +1,21 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var msg = 'File does not exist!';
+  fs.exists('index.html', function(exists) {
+    if (exists) {
+      var indexContent = fs.readFileSync('index.html');
+      if (Buffer.isBuffer(indexContent) {
+        msg = Buffer.toString(indexContent);  
+      } else {
+        msg = indexContent;
+      }
+    }
+    response.send(msg);	
+  }     
 });
 
 var port = process.env.PORT || 5000;
